6c55a4ab2bb9f349775466ed5206596c94afd228
charbin92 <charbin@murraystate.edu>
2018-06-03 17:55:43 -0500

    Created a merge sort algorithm in C#.  Implemented all methods.

---
+++ SortingApps/SortingApps/MergeSort.cs
@@ -0,0 +1,76 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SortingApps
+{
+    public static class MergeSort
+    {
+        public static void DoMergeSort(this int[] numbers)
+        {
+            List<int> sortedNumbers = mergeSort(numbers).ToList();
+
+            for (int i = 0; i < sortedNumbers.Count; i++)
+            {
+                numbers[i] = sortedNumbers[i];
+            }
+        }
+
+        private static int[] mergeSort(int[] numbers)
+        {
+            if (numbers.Length <= 1) // Base Case
+                return numbers;
+
+            List<int> left = new List<int>();
+            List<int> right = new List<int>();
+
+            for (int i = 0; i < numbers.Length; i++)
+            {
+                if (i%2 == 0)
+                    right.Add(numbers[i]);
+                else
+                    left.Add(numbers[i]);
+            }
+
+            // Recursively sort bot sublists.
+            right = mergeSort(right.ToArray()).ToList();
+            left = mergeSort(left.ToArray()).ToList();
+
+            return merge(right, left);
+        }
+
+        private static int[] merge(List<int> right, List<int> left)
+        {
+            List<int> result = new List<int>();
+
+            while (notEmpty(right) && notEmpty(left))
+            {
+                if (right.First() <= left.First())
+                    moveValueFromSourceToResult(right, result);
+                else
+                    moveValueFromSourceToResult(left, result);
+            }
+
+            while(notEmpty(right))
+                moveValueFromSourceToResult(right, result);
+
+            while(notEmpty(left))
+                moveValueFromSourceToResult(left, result);
+
+            return result.ToArray();
+        }
+
+        private static bool notEmpty(List<int> list)
+        {
+            return list.Count > 0;
+        }
+
+        private static void moveValueFromSourceToResult(List<int> list, List<int> result)
+        {
+            result.Add(list.First());
+            list.RemoveAt(0);
+        }
+    }
+}
---
+++ SortingApps/SortingApps/SortingApp.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using static System.Console;
+
+namespace SortingApps
+{
+    class SortingApp
+    {
+        static void Main(string[] args)
+        {
+            testMergeSort();
+
+            ReadKey();
+        }
+
+        private static void testMergeSort()
+        {
+            int[] numbers = { 1, 4, 9, 7, 3, 5, 1, 75, 89, 3, 7 };
+
+            foreach (int number in numbers)
+            {
+                Write(number + " ");
+            }
+
+            WriteLine();
+            WriteLine("===============================");
+            WriteLine();
+
+            numbers.DoMergeSort();
+
+            foreach (int number in numbers)
+            {
+                Write(number + " ");
+            }
+        }
+    }
+}
