f49deb80144a11f7af18e975d04654d078accd40
charbin92 <charbin@murraystate.edu>
2018-06-03 19:46:10 -0500

    Refactored Bubble Sort
    Cleaned up the code that implements the Bubble sort Algorithm and tested
    it in the main program.

--- SortingApps/SortingApps/SortingApp.cs
+++ SortingApps/SortingApps/SortingApp.cs
@@ -12,6 +12,7 @@ namespace SortingAlgorithms
         static void Main(string[] args)
         {
             testMergeSort();
+            testBubbleSort();
 
             ReadKey();
         }
@@ -19,6 +20,7 @@ namespace SortingAlgorithms
         private static void testMergeSort()
         {
             int[] numbers = { 1, 4, 9, 7, 3, 5, 1, 75, 89, 3, 7 };
+            WriteLine("Testing Merge Sort");
 
             foreach (int number in numbers)
             {
@@ -35,6 +37,35 @@ namespace SortingAlgorithms
             {
                 Write(number + " ");
             }
+            WriteLine();
+            WriteLine("================================");
+            WriteLine();
+        }
+
+        private static void testBubbleSort()
+        {
+            int[] numbers = {5, 1, 4, 2, 8};
+            WriteLine("Testing Bubble Sort");
+
+            foreach (int number in numbers)
+            {
+                Write(number + " ");
+            }
+
+            WriteLine();
+            WriteLine("===============================");
+            WriteLine();
+
+            numbers.DoMergeSort();
+
+            foreach (int number in numbers)
+            {
+                Write(number + " ");
+            }
+
+            WriteLine();
+            WriteLine("================================");
+            WriteLine();
         }
     }
 }
